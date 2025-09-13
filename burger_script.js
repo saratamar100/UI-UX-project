0c89cc00ed4bafb873ec3ef8f2eb6378820ae7b4
שרה תמר אמיתי <saratama@g.jct.ac.il>
2025-09-14 00:58:54 +0300

    burger

---
+++ burger_script.js
@@ -0,0 +1,6 @@
+const burger = document.getElementById("burger");
+const navMenu = document.getElementById("nav-menu");
+
+burger.addEventListener("click", () => {
+  navMenu.classList.toggle("active");
+});
